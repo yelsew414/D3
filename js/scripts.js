f4dfe86c2d2790ebcc8659d3f080ccb2eff498ee
yelsew414 <yelsew414@gmail.com>
2022-05-19 14:59:33 -0600

    removed california data. Updated data scource to github csv

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ d3.select("body")
     .attr("cy", 25)
     .attr("r", 25)
     .style("fill", "purple");
+
+var margin = {top: 30, right: 30, bottom: 70, left: 60};
+var width = 400 - margin.top - margin.bottom;
+var height = 400 - margin.top - margin.right;
+
+// var svg = d3.select("#my_dataviz")
+//     .append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//     .append("g")
+//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("https://raw.githubusercontent.com/prasertcbs/basic-dataset/master/college-salaries/degrees-that-pay-back.csv").then(data => {
+    console.log(data)
+});
