5ccd565be5def484cfe04cb07b18e2f7e0c8908c
yelsew414 <yelsew414@gmail.com>
2022-05-26 14:08:00 -0600

    Created script for linechart

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ var margin = {top: 30, right: 30, bottom: 150, left: 60};
 var width = 1000 - margin.left - margin.right;
 var height = 700 - margin.top - margin.bottom;
 
-var svg = d3.select("svg")
+var svg = d3.select("svg#barchart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
